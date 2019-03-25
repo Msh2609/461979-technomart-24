780ff0ebc0f1593405ccf8d95acc5c1ea6d691ab
msh2609 <manon.84@mail.ru>
2019-03-25 07:52:47 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -12,8 +12,41 @@ modalclose.addEventListener('click', function(){
 
 
 
+//slider
 
-//remove
+var slides = document.querySelectorAll('.slider-input');
+var arrowLeft = document.querySelector('.slider-arrows-left');
+var arrowRight = document.querySelector('.slider-arrows-right');
+
+//var activeSlide = document.querySelector('.slider-input').checked
+
+arrowLeft.addEventListener('click', function(){
+  slides.forEach((slide, index) => {
+    if(slide.checked && index === 0) return
+    if(slide.checked) {
+      slides[index - 1].checked = true
+    }
+  })  
+})
+
+arrowRight.addEventListener('click', function(){
+  slides.forEach((slide, index) => {
+    if(slide.checked && slides.length - 1 === index) return
+    if(slide.checked) {
+      slides[index + 1].checked = true
+    }
+  })  
+})
+
+slides.forEach((slide, index) => {
+  console.log(slide.checked)
+})
+
+
+
+
+
+
 
 
 
